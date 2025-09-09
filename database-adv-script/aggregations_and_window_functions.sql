bf940ce30c6c9178e4dac254435418fc26197c97
Julius Maina <205766608+pointerjm@users.noreply.github.com>
2025-09-09 11:45:40 -0400

    section 2 error

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -15,7 +15,18 @@ SELECT
     p.name,
     p.location,
     COUNT(b.booking_id) AS booking_count,
-    ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
+    ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_row_number
+FROM properties p
+LEFT JOIN bookings b ON p.property_id = b.property_id
+GROUP BY p.property_id, p.name, p.location;
+
+-- Query 3: Rank properties based on the total number of bookings using RANK
+SELECT 
+    p.property_id,
+    p.name,
+    p.location,
+    COUNT(b.booking_id) AS booking_count,
+    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
 FROM properties p
 LEFT JOIN bookings b ON p.property_id = b.property_id
 GROUP BY p.property_id, p.name, p.location;
